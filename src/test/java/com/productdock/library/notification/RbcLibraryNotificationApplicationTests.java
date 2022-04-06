6cda392093ddfeac2dea01579cafcd66f8bfb60a
Dusan <dusan.petrovic@productdock.com>
2022-04-06 09:11:45 +0200

    Added initial project setup

---
+++ src/test/java/com/productdock/library/notification/RbcLibraryNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productdock.library.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbcLibraryNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

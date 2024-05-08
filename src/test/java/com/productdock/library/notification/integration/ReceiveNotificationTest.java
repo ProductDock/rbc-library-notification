d3599ee1bab9e95645252c864135541de7259c25
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-05-08 14:18:22 +0200

    Debug test

--- src/test/java/com/productdock/library/notification/integration/ReceiveNotificationTest.java
+++ src/test/java/com/productdock/library/notification/integration/ReceiveNotificationTest.java
@@ -1,15 +1,11 @@
 package com.productdock.library.notification.integration;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.productdock.library.notification.adapter.in.kafka.messages.NotificationMessage;
 import com.productdock.library.notification.adapter.out.mongo.NotificationRepository;
-import com.productdock.library.notification.adapter.out.mongo.enitity.NotificationEntity;
 import com.productdock.library.notification.integration.kafka.KafkaTestBase;
 import com.productdock.library.notification.integration.kafka.KafkaTestProducer;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,12 +13,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.time.Duration;
-import java.util.List;
-
 import static com.productdock.library.notification.data.provider.in.kafka.NotificationMessageMother.notificationMessage;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @Slf4j
@@ -46,19 +37,19 @@ public class ReceiveNotificationTest extends KafkaTestBase {
        // notificationRepository.deleteAll();
     }
 
-    @Test
-    void shouldSaveNotification_whenMessageReceived() throws JsonProcessingException {
-        producer.sendNotificationMessage(topic, NOTIFICATION_MESSAGE);
-
-        await()
-                .atMost(Duration.ofSeconds(20))
-                .until(() -> {
-                    log.info("Fetching...");
-                    List<NotificationEntity> savedNotification = notificationRepository.findAll();
-                    log.info("Fetched notifications:{}", savedNotification);
-                    return !savedNotification.isEmpty();
-                });
-
-        assertThat(notificationRepository.findAllByUserId(NOTIFICATION_MESSAGE.userId)).isNotEmpty();
-    }
+//    @Test
+//    void shouldSaveNotification_whenMessageReceived() throws JsonProcessingException {
+//        producer.sendNotificationMessage(topic, NOTIFICATION_MESSAGE);
+//
+//        await()
+//                .atMost(Duration.ofSeconds(20))
+//                .until(() -> {
+//                    log.info("Fetching...");
+//                    List<NotificationEntity> savedNotification = notificationRepository.findAll();
+//                    log.info("Fetched notifications:{}", savedNotification);
+//                    return !savedNotification.isEmpty();
+//                });
+//
+//        assertThat(notificationRepository.findAllByUserId(NOTIFICATION_MESSAGE.userId)).isNotEmpty();
+//    }
 }

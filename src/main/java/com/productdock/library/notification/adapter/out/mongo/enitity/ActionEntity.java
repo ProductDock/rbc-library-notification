c3a3444b416af06c6a938a137c695502b09ce8a1
Nenad Jeckovic <nenad.jeckovic@productdock.com>
2024-05-10 15:08:45 +0200

    Fix test again

--- src/main/java/com/productdock/library/notification/adapter/out/mongo/enitity/ActionEntity.java
+++ src/main/java/com/productdock/library/notification/adapter/out/mongo/enitity/ActionEntity.java
@@ -3,11 +3,15 @@ package com.productdock.library.notification.adapter.out.mongo.enitity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
-public class ActionEntity {
+public class ActionEntity implements Serializable {
     private String type;
     private String target;
 }
--- src/main/java/com/productdock/library/notification/adapter/out/mongo/enitity/NotificationEntity.java
+++ src/main/java/com/productdock/library/notification/adapter/out/mongo/enitity/NotificationEntity.java
@@ -3,14 +3,18 @@ package com.productdock.library.notification.adapter.out.mongo.enitity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
 
 @Document("notifications")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
-public class NotificationEntity {
+public class NotificationEntity implements Serializable {
     @Id
     private String id;
     private String title;
